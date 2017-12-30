22cdfeaebe26edc58551f8b97f546ae01cb68519
Keith Peters <keith@monkeynova.com>
2017-12-29 23:19:11 -0800

    add two failing "existing active set" tests to demonstrate some bugs

--- puzzle/active_set_builder_test.cc
+++ puzzle/active_set_builder_test.cc
@@ -88,7 +88,7 @@ TEST(ActiveSetBuilderTest, SingleClass) {
   EXPECT_THAT(vector_history.size(), Eq(6));
 }
 
-  TEST(ActiveSetBuilderTest, MultiClass) {
+TEST(ActiveSetBuilderTest, MultiClass) {
   IntRangeDescriptor id_descriptor(0, 2);
   EntryDescriptor entry_descriptor;
   const int kClassIntA = 0;
@@ -145,6 +145,164 @@ TEST(ActiveSetBuilderTest, SingleClass) {
       mutable_solution.SetClass(it_b);
       ++got_iteration_count;
       if (c.p(test_solution)) {
+	++got_found_count;
+      }
+    }
+  }
+  LOG(INFO) << "Iteration change " << full_iteration_count << " => "
+	    << got_iteration_count;
+  EXPECT_THAT(got_iteration_count, Lt(0.5 * full_iteration_count));
+  EXPECT_THAT(got_found_count, Eq(expect_found_count));
+}
+
+TEST(ActiveSetBuilderTest, MultiClassExistingActiveSet) {
+  IntRangeDescriptor id_descriptor(0, 2);
+  EntryDescriptor entry_descriptor;
+  const int kClassIntA = 0;
+  const int kClassIntB = 1;
+  entry_descriptor.SetIds(&id_descriptor);
+  IntRangeDescriptor class_descriptor_a(3, 5);
+  entry_descriptor.SetClass(kClassIntA, "class a", &class_descriptor_a);
+  IntRangeDescriptor class_descriptor_b(3, 5);
+  entry_descriptor.SetClass(kClassIntB, "class b", &class_descriptor_b);
+
+  ActiveSetBuilder builder(&entry_descriptor);
+
+  ClassPermuter permuter_a(&class_descriptor_a, kClassIntA);
+  ClassPermuter permuter_b(&class_descriptor_b, kClassIntB);
+  ASSERT_THAT(permuter_a.permutation_count(), 6);
+  ASSERT_THAT(permuter_b.permutation_count(), 6);
+
+  Solution::Cropper a_cropper("a is 4 for id 1",
+			      [](const Solution& s) {
+				return s.Id(1).Class(kClassIntA) == 4;
+			      },
+			      {kClassIntA});
+
+  ActiveSet active_set_a_pre = builder.Build(permuter_a, {a_cropper});
+  permuter_a.set_active_set(active_set_a_pre);
+  
+  ActiveSet active_set_a_post;
+  ActiveSet active_set_b;
+
+  Solution::Cropper pair_cropper("a is 3 and b is 4 for id 0",
+				 [](const Solution& s) {
+				   return s.Id(0).Class(kClassIntA) == 3 &&
+				     s.Id(0).Class(kClassIntB) == 4;
+				 },
+				 {kClassIntA, kClassIntB});
+  
+  builder.Build(permuter_a, permuter_b, {pair_cropper},
+		&active_set_a_post, &active_set_b);
+
+  MutableSolution mutable_solution(&entry_descriptor);
+  Solution test_solution = mutable_solution.TestableSolution();
+
+  int expect_found_count = 0;
+  int full_iteration_count = 0;
+  for (auto it_a = permuter_a.begin(); it_a != permuter_a.end(); ++it_a) {
+    mutable_solution.SetClass(it_a);
+    EXPECT_TRUE(a_cropper.p(test_solution));
+    for (auto it_b = permuter_b.begin(); it_b != permuter_b.end(); ++it_b) {
+      mutable_solution.SetClass(it_b);
+      ++full_iteration_count;
+      if (pair_cropper.p(test_solution)) {
+	++expect_found_count;
+      }
+    }
+  }
+
+  permuter_a.set_active_set(active_set_a_post);
+  permuter_b.set_active_set(active_set_b);
+
+  int got_found_count = 0;
+  int got_iteration_count = 0;
+  for (auto it_a = permuter_a.begin(); it_a != permuter_a.end(); ++it_a) {
+    mutable_solution.SetClass(it_a);
+    EXPECT_TRUE(a_cropper.p(test_solution));
+    for (auto it_b = permuter_b.begin(); it_b != permuter_b.end(); ++it_b) {
+      mutable_solution.SetClass(it_b);
+      ++got_iteration_count;
+      if (pair_cropper.p(test_solution)) {
+	++got_found_count;
+      }
+    }
+  }
+  LOG(INFO) << "Iteration change " << full_iteration_count << " => "
+	    << got_iteration_count;
+  EXPECT_THAT(got_iteration_count, Lt(0.5 * full_iteration_count));
+  EXPECT_THAT(got_found_count, Eq(expect_found_count));
+}
+
+TEST(ActiveSetBuilderTest, MultiClassExistingActiveSetForB) {
+  IntRangeDescriptor id_descriptor(0, 2);
+  EntryDescriptor entry_descriptor;
+  const int kClassIntA = 0;
+  const int kClassIntB = 1;
+  entry_descriptor.SetIds(&id_descriptor);
+  IntRangeDescriptor class_descriptor_a(3, 5);
+  entry_descriptor.SetClass(kClassIntA, "class a", &class_descriptor_a);
+  IntRangeDescriptor class_descriptor_b(3, 5);
+  entry_descriptor.SetClass(kClassIntB, "class b", &class_descriptor_b);
+
+  ActiveSetBuilder builder(&entry_descriptor);
+
+  ClassPermuter permuter_a(&class_descriptor_a, kClassIntA);
+  ClassPermuter permuter_b(&class_descriptor_b, kClassIntB);
+  ASSERT_THAT(permuter_a.permutation_count(), 6);
+  ASSERT_THAT(permuter_b.permutation_count(), 6);
+
+  Solution::Cropper b_cropper("b is 4 for id 1",
+			      [](const Solution& s) {
+				return s.Id(1).Class(kClassIntB) == 5;
+			      },
+			      {kClassIntB});
+
+  ActiveSet active_set_b_pre = builder.Build(permuter_b, {b_cropper});
+  permuter_b.set_active_set(active_set_b_pre);
+  
+  ActiveSet active_set_a;
+  ActiveSet active_set_b_post;
+
+  Solution::Cropper pair_cropper("a is 3 and b is 4 for id 0",
+				 [](const Solution& s) {
+				   return s.Id(0).Class(kClassIntA) == 3 &&
+				     s.Id(0).Class(kClassIntB) == 4;
+				 },
+				 {kClassIntA, kClassIntB});
+  
+  builder.Build(permuter_a, permuter_b, {pair_cropper},
+		&active_set_a, &active_set_b_post);
+
+  MutableSolution mutable_solution(&entry_descriptor);
+  Solution test_solution = mutable_solution.TestableSolution();
+
+  int expect_found_count = 0;
+  int full_iteration_count = 0;
+  for (auto it_a = permuter_a.begin(); it_a != permuter_a.end(); ++it_a) {
+    mutable_solution.SetClass(it_a);
+    for (auto it_b = permuter_b.begin(); it_b != permuter_b.end(); ++it_b) {
+      mutable_solution.SetClass(it_b);
+      EXPECT_TRUE(b_cropper.p(test_solution));
+      ++full_iteration_count;
+      if (pair_cropper.p(test_solution)) {
+	++expect_found_count;
+      }
+    }
+  }
+
+  permuter_a.set_active_set(active_set_a);
+  permuter_b.set_active_set(active_set_b_post);
+
+  int got_found_count = 0;
+  int got_iteration_count = 0;
+  for (auto it_a = permuter_a.begin(); it_a != permuter_a.end(); ++it_a) {
+    mutable_solution.SetClass(it_a);
+    for (auto it_b = permuter_b.begin(); it_b != permuter_b.end(); ++it_b) {
+      mutable_solution.SetClass(it_b);
+      EXPECT_TRUE(b_cropper.p(test_solution));
+      ++got_iteration_count;
+      if (pair_cropper.p(test_solution)) {
 	++got_found_count;
       }
     }
