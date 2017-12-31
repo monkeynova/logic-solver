825248f83b973c1940ed160dafa4a3acac9c888f
Keith Peters <keith@monkeynova.com>
2017-12-30 17:01:33 -0800

    template puzzle_test benchmarks on major allowed options

--- puzzle_test.cc
+++ puzzle_test.cc
@@ -2,10 +2,15 @@
 #include <memory>
 #include <vector>
 
+#include "absl/strings/str_join.h"
 #include "benchmark/benchmark.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "puzzle/solver.h"
+
+DECLARE_bool(puzzle_brute_force);
+DECLARE_bool(puzzle_prune_class_iterator);
+DECLARE_bool(puzzle_prune_reorder_classes);
 
 extern void SetupProblem(puzzle::Solver* s);
 extern puzzle::Solution ProblemSolution(const puzzle::Solver& s);
@@ -21,14 +26,37 @@ TEST(Puzzle, RightAnswer) {
   EXPECT_EQ(got, expect);
 }
 
+template <bool brute, bool prune, bool reorder>
 static void BM_Solver(benchmark::State& state) {
   puzzle::Solver solver;
   SetupProblem(&solver);
+
+  std::vector<std::string> label;
+  if (brute) {
+    FLAGS_puzzle_brute_force = true;
+    label.push_back("brute");
+  }
+  if (prune) {
+    FLAGS_puzzle_prune_class_iterator = true;
+    label.push_back("prune");
+  }
+  if (reorder) {
+    FLAGS_puzzle_prune_reorder_classes = true;
+    label.push_back("reorder");
+  }
+  state.SetLabel(absl::StrJoin(label, " "));
 
   for (auto _ : state) {
     solver.Solve();
   }
 }
 
-BENCHMARK(BM_Solver);
+BENCHMARK_TEMPLATE(BM_Solver, /*brute=*/false, /*prune=*/false,
+		   /*reorder=*/false);
+BENCHMARK_TEMPLATE(BM_Solver, /*brute=*/true, /*prune=*/false,
+		   /*reorder=*/false);
+BENCHMARK_TEMPLATE(BM_Solver, /*brute=*/false, /*prune=*/true,
+		   /*reorder=*/false);
+BENCHMARK_TEMPLATE(BM_Solver, /*brute=*/false, /*prune=*/true,
+		   /*reorder=*/true);
 
