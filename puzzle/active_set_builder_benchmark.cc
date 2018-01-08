97ca64f8d9a5ed517c0c999f7ed0093a08c34f3b
Keith Peters <keith@monkeynova.com>
2018-01-07 21:36:35 -0800

    extract performance methods so I can call two at once to verify behavior

--- puzzle/active_set_builder_benchmark.cc
+++ puzzle/active_set_builder_benchmark.cc
@@ -14,8 +14,9 @@ struct SetupState {
     kMakePairs = 3,
   };
 
-  explicit SetupState(int permutation_count)
-    : descriptor(MakeDescriptor(permutation_count, &owned_descriptors)),
+  SetupState(int permutation_count, BuildMode build_mode_in)
+    : build_mode(build_mode_in),
+      descriptor(MakeDescriptor(permutation_count, &owned_descriptors)),
       mutable_solution(&descriptor),
       solution(mutable_solution.TestableSolution()),
       predicates({MakePairCropper()}),
@@ -75,6 +76,11 @@ struct SetupState {
     return ret;
   }
 
+  void BuildPairSet();
+  void BuildPassThroughA();
+  void BuildBackAndForth();
+
+  BuildMode build_mode;
   std::vector<std::unique_ptr<Descriptor>> owned_descriptors;
   EntryDescriptor descriptor;
   MutableSolution mutable_solution;
@@ -88,231 +94,227 @@ struct SetupState {
 
 template <SetupState::BuildMode build_mode>
 static void BM_PairSet(benchmark::State& state) {
-  SetupState setup(state.range(0));
+  SetupState setup(state.range(0), build_mode);
 
   for (auto _ : state) {
-    std::set<int> a_match_positions;
-    std::set<int> b_match_positions;
-    std::map<int, std::set<int>> a_b_match_positions;
-    std::map<int, std::set<int>> b_a_match_positions;
+    setup.BuildPairSet();
+  }
+}
 
-    for (auto it_a = setup.permuter_a.begin();
-         it_a != setup.permuter_a.end();
-         ++it_a) {
-      setup.mutable_solution.SetClass(it_a);
-      for (auto it_b = setup.permuter_b.begin();
-           it_b != setup.permuter_b.end();
-           ++it_b) {
-        if (build_mode == SetupState::kEarlyExit &&
-            a_match_positions.count(it_a.position()) > 0 &&
-            b_match_positions.count(it_b.position()) > 0) {
-          // Already added both pieces.
-          continue;
-        }
-        setup.mutable_solution.SetClass(it_b);
-        const bool this_match = std::all_of(setup.predicates.begin(),
-                                            setup.predicates.end(),
-                                            [&setup](const Solution::Cropper& c) {
-                                              return c.p(setup.solution);
-                                            });
-        if (this_match) {
-          a_match_positions.insert(it_a.position());
-          b_match_positions.insert(it_b.position());
-          if (build_mode == SetupState::kMakePairs) {
-            a_b_match_positions[it_a.position()].insert(it_b.position());
-            b_a_match_positions[it_b.position()].insert(it_a.position());
-          }
+void SetupState::BuildPairSet() {
+  std::set<int> a_match_positions;
+  std::set<int> b_match_positions;
+  std::map<int, std::set<int>> a_b_match_positions;
+  std::map<int, std::set<int>> b_a_match_positions;
+
+  for (auto it_a = permuter_a.begin(); it_a != permuter_a.end(); ++it_a) {
+    mutable_solution.SetClass(it_a);
+    for (auto it_b = permuter_b.begin(); it_b != permuter_b.end(); ++it_b) {
+      if (build_mode == SetupState::kEarlyExit &&
+          a_match_positions.count(it_a.position()) > 0 &&
+          b_match_positions.count(it_b.position()) > 0) {
+        // Already added both pieces.
+        continue;
+      }
+      mutable_solution.SetClass(it_b);
+      const bool this_match = std::all_of(predicates.begin(),
+                                          predicates.end(),
+                                          [this](const Solution::Cropper& c) {
+                                            return c.p(solution);
+                                          });
+      if (this_match) {
+        a_match_positions.insert(it_a.position());
+        b_match_positions.insert(it_b.position());
+        if (build_mode == SetupState::kMakePairs) {
+          a_b_match_positions[it_a.position()].insert(it_b.position());
+          b_a_match_positions[it_b.position()].insert(it_a.position());
         }
       }
     }
+  }
 
-    setup.active_sets[setup.permuter_a.class_int()] = ActiveSet(
-        a_match_positions, setup.permuter_a.permutation_count());
-    setup.active_sets[setup.permuter_b.class_int()] = ActiveSet(
-        b_match_positions, setup.permuter_b.permutation_count());
-    if (build_mode == SetupState::kMakePairs) {
-      for (const auto& pair : a_b_match_positions) {
-        const int a_val = pair.first;
-        const std::set<int>& b_set = pair.second;
-        setup.active_set_pairs[SetupState::kClassIntA][a_val] = ActiveSet(
-               b_set, setup.permuter_b.permutation_count());
-      }
-      for (const auto& pair : b_a_match_positions) {
-        const int b_val = pair.first;
-        const std::set<int>& a_set = pair.second;
-        setup.active_set_pairs[SetupState::kClassIntB][b_val] = ActiveSet(
-              a_set, setup.permuter_a.permutation_count());
-      }
+  active_sets[permuter_a.class_int()] = ActiveSet(
+      a_match_positions, permuter_a.permutation_count());
+  active_sets[permuter_b.class_int()] = ActiveSet(
+      b_match_positions, permuter_b.permutation_count());
+  if (build_mode == SetupState::kMakePairs) {
+    for (const auto& pair : a_b_match_positions) {
+      const int a_val = pair.first;
+      const std::set<int>& b_set = pair.second;
+      active_set_pairs[SetupState::kClassIntA][a_val] = ActiveSet(
+          b_set, permuter_b.permutation_count());
+    }
+    for (const auto& pair : b_a_match_positions) {
+      const int b_val = pair.first;
+      const std::set<int>& a_set = pair.second;
+      active_set_pairs[SetupState::kClassIntB][b_val] = ActiveSet(
+          a_set, permuter_a.permutation_count());
     }
   }
 }
 
 template <SetupState::BuildMode build_mode>
 static void BM_PairPassThroughA(benchmark::State& state) {
-  SetupState setup(state.range(0));
+  SetupState setup(state.range(0), build_mode);
 
   for (auto _ : state) {
-    ActiveSet active_set_a = ActiveSet();
-    ActiveSet source_a = setup.permuter_a.active_set();
-    std::set<int> b_match_positions;
-    std::map<int, std::set<int>> b_a_match_positions;
+    setup.BuildPassThroughA();
+  }
+}
 
-    for (auto it_a = setup.permuter_a.begin();
-         it_a != setup.permuter_a.end();
-         ++it_a) {
-      setup.mutable_solution.SetClass(it_a);
-      active_set_a.AddFalseBlock(source_a.ConsumeFalseBlock());
-      CHECK(source_a.ConsumeNext());
-      bool any_of_a = false;
-      ActiveSet a_b_set;
-      ActiveSet source_b = setup.permuter_b.active_set();
-      for (auto it_b = setup.permuter_b.begin();
-           it_b != setup.permuter_b.end();
-           ++it_b) {
-        if (build_mode == SetupState::kEarlyExit && any_of_a &&
-            b_match_positions.find(it_b.position()) != b_match_positions.end()) {
-          // Already added both pieces.
-          continue;
-        }
-        setup.mutable_solution.SetClass(it_b);
-        const bool this_match = std::all_of(setup.predicates.begin(),
-                                            setup.predicates.end(),
-                                            [&setup](const Solution::Cropper& c) {
-                                              return c.p(setup.solution);
-                                            });
-        if (this_match) {
-          any_of_a = true;
-          b_match_positions.insert(it_b.position());
-        }
-        if (build_mode == SetupState::kMakePairs) {
-          a_b_set.AddFalseBlock(source_b.ConsumeFalseBlock());
-          CHECK(source_b.ConsumeNext());
-          a_b_set.Add(this_match);
-          if (this_match) {
-            b_a_match_positions[it_b.position()].insert(it_a.position());
-          }
-        }
-      }
-      if (build_mode == SetupState::kMakePairs && any_of_a) {
-        a_b_set.DoneAdding();
-        setup.active_set_pairs[SetupState::kClassIntA][it_a.position()] =
-          std::move(a_b_set);
-      }
-      active_set_a.Add(any_of_a);
-    }
+void SetupState::BuildPassThroughA() {
+  ActiveSet active_set_a = ActiveSet();
+  ActiveSet source_a = permuter_a.active_set();
+  std::set<int> b_match_positions;
+  std::map<int, std::set<int>> b_a_match_positions;
 
+  for (auto it_a = permuter_a.begin(); it_a != permuter_a.end(); ++it_a) {
+    mutable_solution.SetClass(it_a);
     active_set_a.AddFalseBlock(source_a.ConsumeFalseBlock());
     CHECK(source_a.ConsumeNext());
-    active_set_a.DoneAdding();
-    setup.active_sets[setup.permuter_a.class_int()] = std::move(active_set_a);
-    setup.active_sets[setup.permuter_b.class_int()] = ActiveSet(
-        b_match_positions, setup.permuter_b.permutation_count());
-    if (build_mode == SetupState::kMakePairs) {
-      for (const auto& pair : b_a_match_positions) {
-        const int b_val = pair.first;
-        const std::set<int>& a_set = pair.second;
-        setup.active_set_pairs[SetupState::kClassIntB][b_val] = ActiveSet(
-                a_set, setup.permuter_a.permutation_count());
+    bool any_of_a = false;
+    ActiveSet a_b_set;
+    ActiveSet source_b = permuter_b.active_set();
+    for (auto it_b = permuter_b.begin(); it_b != permuter_b.end(); ++it_b) {
+      if (build_mode == SetupState::kEarlyExit && any_of_a &&
+          b_match_positions.find(it_b.position()) != b_match_positions.end()) {
+        // Already added both pieces.
+        continue;
       }
+      mutable_solution.SetClass(it_b);
+      const bool this_match = std::all_of(predicates.begin(),
+                                          predicates.end(),
+                                          [this](const Solution::Cropper& c) {
+                                            return c.p(solution);
+                                          });
+      if (this_match) {
+        any_of_a = true;
+        b_match_positions.insert(it_b.position());
+      }
+      if (build_mode == SetupState::kMakePairs) {
+        a_b_set.AddFalseBlock(source_b.ConsumeFalseBlock());
+        CHECK(source_b.ConsumeNext());
+        a_b_set.Add(this_match);
+        if (this_match) {
+          b_a_match_positions[it_b.position()].insert(it_a.position());
+        }
+      }
+    }
+    if (build_mode == SetupState::kMakePairs && any_of_a) {
+      a_b_set.DoneAdding();
+      active_set_pairs[SetupState::kClassIntA][it_a.position()] =
+        std::move(a_b_set);
+    }
+    active_set_a.Add(any_of_a);
+  }
+
+  active_set_a.AddFalseBlock(source_a.ConsumeFalseBlock());
+  CHECK(source_a.ConsumeNext());
+  active_set_a.DoneAdding();
+  active_sets[permuter_a.class_int()] = std::move(active_set_a);
+  active_sets[permuter_b.class_int()] = ActiveSet(
+      b_match_positions, permuter_b.permutation_count());
+  if (build_mode == SetupState::kMakePairs) {
+    for (const auto& pair : b_a_match_positions) {
+      const int b_val = pair.first;
+      const std::set<int>& a_set = pair.second;
+      active_set_pairs[SetupState::kClassIntB][b_val] = ActiveSet(
+          a_set, permuter_a.permutation_count());
     }
   }
 }
 
 template <SetupState::BuildMode build_mode>
 static void BM_PairBackAndForth(benchmark::State& state) {
-  SetupState setup(state.range(0));
+  SetupState setup(state.range(0), build_mode);
 
   for (auto _ : state) {
-    {
-      ActiveSet active_set_a = ActiveSet();
-      ActiveSet source_a = setup.permuter_a.active_set();
+    setup.BuildBackAndForth();
+  }
+}
 
-      for (auto it_a = setup.permuter_a.begin();
-           it_a != setup.permuter_a.end();
-           ++it_a) {
-        setup.mutable_solution.SetClass(it_a);
-        active_set_a.AddFalseBlock(source_a.ConsumeFalseBlock());
-        CHECK(source_a.ConsumeNext());
-        bool any_of_a = false;
-        ActiveSet a_b_set;
-        ActiveSet source_b = setup.permuter_b.active_set();
-        for (auto it_b = setup.permuter_b.begin();
-             it_b != setup.permuter_b.end();
-             ++it_b) {
-          setup.mutable_solution.SetClass(it_b);
-          const bool this_match = std::all_of(setup.predicates.begin(),
-                                              setup.predicates.end(),
-                                              [&setup](const Solution::Cropper& c) {
-                                                return c.p(setup.solution);
-                                              });
-          if (this_match) {
-            any_of_a = true;
-            if (build_mode == SetupState::kEarlyExit) break;
-          }
-          if (build_mode == SetupState::kMakePairs) {
-            a_b_set.AddFalseBlock(source_b.ConsumeFalseBlock());
-            CHECK(source_b.ConsumeNext());
-            a_b_set.Add(this_match);
-          }
-        }
-        active_set_a.Add(any_of_a);
-        if (build_mode == SetupState::kMakePairs && any_of_a) {
-          a_b_set.DoneAdding();
-          setup.active_set_pairs[SetupState::kClassIntA][it_a.position()] =
-            std::move(a_b_set);
-        }
-      }
+void SetupState::BuildBackAndForth() {
+  {
+    ActiveSet active_set_a = ActiveSet();
+    ActiveSet source_a = permuter_a.active_set();
 
+    for (auto it_a = permuter_a.begin(); it_a != permuter_a.end(); ++it_a) {
+      mutable_solution.SetClass(it_a);
       active_set_a.AddFalseBlock(source_a.ConsumeFalseBlock());
       CHECK(source_a.ConsumeNext());
-      active_set_a.DoneAdding();
-      setup.active_sets[setup.permuter_a.class_int()] = std::move(active_set_a);
-    }
-    {
-      ActiveSet active_set_b = ActiveSet();
-      ActiveSet source_b = setup.permuter_b.active_set();
-
-      for (auto it_b = setup.permuter_b.begin();
-           it_b != setup.permuter_b.end();
-           ++it_b) {
-        setup.mutable_solution.SetClass(it_b);
-        active_set_b.AddFalseBlock(source_b.ConsumeFalseBlock());
-        CHECK(source_b.ConsumeNext());
-        ActiveSet b_a_set;
-        ActiveSet source_a = setup.permuter_a.active_set();
-        bool any_of_b = false;
-        for (auto it_a = setup.permuter_a.begin();
-             it_a != setup.permuter_a.end();
-             ++it_a) {
-          setup.mutable_solution.SetClass(it_a);
-          const bool this_match = std::all_of(setup.predicates.begin(),
-                                              setup.predicates.end(),
-                                              [&setup](const Solution::Cropper& c) {
-                                                return c.p(setup.solution);
-                                              });
-          if (this_match) {
-            any_of_b = true;
-            if (build_mode == SetupState::kEarlyExit) break;
-          }
-          if (build_mode == SetupState::kMakePairs) {
-            b_a_set.AddFalseBlock(source_a.ConsumeFalseBlock());
-            CHECK(source_a.ConsumeNext());
-            b_a_set.Add(this_match);
-          }
+      bool any_of_a = false;
+      ActiveSet a_b_set;
+      ActiveSet source_b = permuter_b.active_set();
+      for (auto it_b = permuter_b.begin(); it_b != permuter_b.end(); ++it_b) {
+        mutable_solution.SetClass(it_b);
+        const bool this_match = std::all_of(predicates.begin(),
+                                            predicates.end(),
+                                            [this](const Solution::Cropper& c) {
+                                              return c.p(solution);
+                                            });
+        if (this_match) {
+          any_of_a = true;
+          if (build_mode == SetupState::kEarlyExit) break;
         }
-        active_set_b.Add(any_of_b);
-        if (build_mode == SetupState::kMakePairs && any_of_b) {
-          b_a_set.DoneAdding();
-          setup.active_set_pairs[SetupState::kClassIntB][it_b.position()] =
-            std::move(b_a_set);
+        if (build_mode == SetupState::kMakePairs) {
+          a_b_set.AddFalseBlock(source_b.ConsumeFalseBlock());
+          CHECK(source_b.ConsumeNext());
+          a_b_set.Add(this_match);
         }
       }
+      active_set_a.Add(any_of_a);
+      if (build_mode == SetupState::kMakePairs && any_of_a) {
+        a_b_set.DoneAdding();
+        active_set_pairs[SetupState::kClassIntA][it_a.position()] =
+          std::move(a_b_set);
+      }
+    }
 
+    active_set_a.AddFalseBlock(source_a.ConsumeFalseBlock());
+    CHECK(source_a.ConsumeNext());
+    active_set_a.DoneAdding();
+    active_sets[permuter_a.class_int()] = std::move(active_set_a);
+  }
+  {
+    ActiveSet active_set_b = ActiveSet();
+    ActiveSet source_b = permuter_b.active_set();
+
+    for (auto it_b = permuter_b.begin(); it_b != permuter_b.end(); ++it_b) {
+      mutable_solution.SetClass(it_b);
       active_set_b.AddFalseBlock(source_b.ConsumeFalseBlock());
       CHECK(source_b.ConsumeNext());
-      active_set_b.DoneAdding();
-      setup.active_sets[setup.permuter_b.class_int()] = std::move(active_set_b);
+      ActiveSet b_a_set;
+      ActiveSet source_a = permuter_a.active_set();
+      bool any_of_b = false;
+      for (auto it_a = permuter_a.begin(); it_a != permuter_a.end(); ++it_a) {
+        mutable_solution.SetClass(it_a);
+        const bool this_match = std::all_of(predicates.begin(),
+                                            predicates.end(),
+                                            [this](const Solution::Cropper& c) {
+                                              return c.p(solution);
+                                            });
+        if (this_match) {
+          any_of_b = true;
+          if (build_mode == SetupState::kEarlyExit) break;
+        }
+        if (build_mode == SetupState::kMakePairs) {
+          b_a_set.AddFalseBlock(source_a.ConsumeFalseBlock());
+          CHECK(source_a.ConsumeNext());
+          b_a_set.Add(this_match);
+        }
+      }
+      active_set_b.Add(any_of_b);
+      if (build_mode == SetupState::kMakePairs && any_of_b) {
+        b_a_set.DoneAdding();
+        active_set_pairs[SetupState::kClassIntB][it_b.position()] =
+          std::move(b_a_set);
+      }
     }
+
+    active_set_b.AddFalseBlock(source_b.ConsumeFalseBlock());
+    CHECK(source_b.ConsumeNext());
+    active_set_b.DoneAdding();
+    active_sets[permuter_b.class_int()] = std::move(active_set_b);
   }
 }
 
