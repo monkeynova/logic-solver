63b7d1b56e78fa8c1477f512d806a918aedd43e1
Keith Peters <keith@monkeynova.com>
2018-01-07 21:24:52 -0800

    reduce full permutation on active_set_builder_benchmark

--- puzzle/active_set_builder_benchmark.cc
+++ puzzle/active_set_builder_benchmark.cc
@@ -13,16 +13,22 @@ struct SetupState {
     kFullIteration = 2,
     kMakePairs = 3,
   };
-  
-  SetupState(int permutation_count)
+
+  explicit SetupState(int permutation_count)
     : descriptor(MakeDescriptor(permutation_count, &owned_descriptors)),
       mutable_solution(&descriptor),
       solution(mutable_solution.TestableSolution()),
-      predicates({MakeCropper()}),
+      predicates({MakePairCropper()}),
       permuter_a(descriptor.AllClassValues(kClassIntA), kClassIntA),
       permuter_b(descriptor.AllClassValues(kClassIntB), kClassIntB),
       active_sets(descriptor.num_classes()),
-      active_set_pairs(descriptor.num_classes()) {}
+      active_set_pairs(descriptor.num_classes()) {
+    ActiveSetBuilder single_class_builder(&descriptor);
+    single_class_builder.Build(permuter_a, {MakeCropperA()});
+    permuter_a.set_active_set(single_class_builder.active_set(kClassIntA));
+    single_class_builder.Build(permuter_b, {MakeCropperB()});
+    permuter_b.set_active_set(single_class_builder.active_set(kClassIntB));
+  }
 
   static EntryDescriptor MakeDescriptor(
       int permutation_count,
@@ -35,17 +41,37 @@ struct SetupState {
     return ret;
   }
 
-  static Solution::Cropper MakeCropper() {
+  static Solution::Cropper MakePairCropper() {
     Solution::Cropper ret(
-	"no entry the same",
-	[](const Solution& s) {
-	  return all_of(s.entries().begin(),
-			s.entries().end(),
+        "no entry the same",
+        [](const Solution& s) {
+          return all_of(s.entries().begin(),
+                        s.entries().end(),
                               [](const Entry& e) {
-			  return e.Class(kClassIntA) != e.Class(kClassIntB);
-			});
-	},
-	{kClassIntA, kClassIntB});
+                          return e.Class(kClassIntA) != e.Class(kClassIntB);
+                        });
+        },
+        {kClassIntA, kClassIntB});
+    return ret;
+  }
+
+  static Solution::Cropper MakeCropperA() {
+    Solution::Cropper ret(
+        "no entry the same",
+        [](const Solution& s) {
+          return s.Id(0).Class(kClassIntA) == 0 && s.Id(1).Class(kClassIntA) == 1;
+        },
+        {kClassIntA});
+    return ret;
+  }
+
+  static Solution::Cropper MakeCropperB() {
+    Solution::Cropper ret(
+        "no entry the same",
+        [](const Solution& s) {
+          return s.Id(0).Class(kClassIntB) == 1 && s.Id(1).Class(kClassIntB) == 0;
+        },
+        {kClassIntB});
     return ret;
   }
 
@@ -77,12 +103,12 @@ static void BM_PairSet(benchmark::State& state) {
       for (auto it_b = setup.permuter_b.begin();
            it_b != setup.permuter_b.end();
            ++it_b) {
-	if (build_mode == SetupState::kEarlyExit &&
-	    a_match_positions.count(it_a.position()) > 0 &&
-	    b_match_positions.count(it_b.position()) > 0) {
+        if (build_mode == SetupState::kEarlyExit &&
+            a_match_positions.count(it_a.position()) > 0 &&
+            b_match_positions.count(it_b.position()) > 0) {
           // Already added both pieces.
-	  continue;
-	}
+          continue;
+        }
         setup.mutable_solution.SetClass(it_b);
         const bool this_match = std::all_of(setup.predicates.begin(),
                                             setup.predicates.end(),
@@ -92,10 +118,10 @@ static void BM_PairSet(benchmark::State& state) {
         if (this_match) {
           a_match_positions.insert(it_a.position());
           b_match_positions.insert(it_b.position());
-	  if (build_mode == SetupState::kMakePairs) {
-	    a_b_match_positions[it_a.position()].insert(it_b.position());
-	    b_a_match_positions[it_b.position()].insert(it_a.position());
-	  }
+          if (build_mode == SetupState::kMakePairs) {
+            a_b_match_positions[it_a.position()].insert(it_b.position());
+            b_a_match_positions[it_b.position()].insert(it_a.position());
+          }
         }
       }
     }
@@ -106,16 +132,16 @@ static void BM_PairSet(benchmark::State& state) {
         b_match_positions, setup.permuter_b.permutation_count());
     if (build_mode == SetupState::kMakePairs) {
       for (const auto& pair : a_b_match_positions) {
-	const int a_val = pair.first;
-	const std::set<int>& b_set = pair.second;
-	setup.active_set_pairs[SetupState::kClassIntA][a_val] = ActiveSet(
-   	    b_set, setup.permuter_b.permutation_count());
+        const int a_val = pair.first;
+        const std::set<int>& b_set = pair.second;
+        setup.active_set_pairs[SetupState::kClassIntA][a_val] = ActiveSet(
+               b_set, setup.permuter_b.permutation_count());
       }
       for (const auto& pair : b_a_match_positions) {
-	const int b_val = pair.first;
-	const std::set<int>& a_set = pair.second;
-	setup.active_set_pairs[SetupState::kClassIntB][b_val] = ActiveSet(
-  	    a_set, setup.permuter_a.permutation_count());
+        const int b_val = pair.first;
+        const std::set<int>& a_set = pair.second;
+        setup.active_set_pairs[SetupState::kClassIntB][b_val] = ActiveSet(
+              a_set, setup.permuter_a.permutation_count());
       }
     }
   }
@@ -157,20 +183,20 @@ static void BM_PairPassThroughA(benchmark::State& state) {
         if (this_match) {
           any_of_a = true;
           b_match_positions.insert(it_b.position());
-	}
-	if (build_mode == SetupState::kMakePairs) {
-	  a_b_set.AddFalseBlock(source_b.ConsumeFalseBlock());
-	  CHECK(source_b.ConsumeNext());
-	  a_b_set.Add(this_match);
-	  if (this_match) {
-	    b_a_match_positions[it_b.position()].insert(it_a.position());
-	  }
+        }
+        if (build_mode == SetupState::kMakePairs) {
+          a_b_set.AddFalseBlock(source_b.ConsumeFalseBlock());
+          CHECK(source_b.ConsumeNext());
+          a_b_set.Add(this_match);
+          if (this_match) {
+            b_a_match_positions[it_b.position()].insert(it_a.position());
+          }
         }
       }
       if (build_mode == SetupState::kMakePairs && any_of_a) {
-	a_b_set.DoneAdding();
-	setup.active_set_pairs[SetupState::kClassIntA][it_a.position()] =
-	  std::move(a_b_set);
+        a_b_set.DoneAdding();
+        setup.active_set_pairs[SetupState::kClassIntA][it_a.position()] =
+          std::move(a_b_set);
       }
       active_set_a.Add(any_of_a);
     }
@@ -183,10 +209,10 @@ static void BM_PairPassThroughA(benchmark::State& state) {
         b_match_positions, setup.permuter_b.permutation_count());
     if (build_mode == SetupState::kMakePairs) {
       for (const auto& pair : b_a_match_positions) {
-	const int b_val = pair.first;
-	const std::set<int>& a_set = pair.second;
-	setup.active_set_pairs[SetupState::kClassIntB][b_val] = ActiveSet(
-    	    a_set, setup.permuter_a.permutation_count());
+        const int b_val = pair.first;
+        const std::set<int>& a_set = pair.second;
+        setup.active_set_pairs[SetupState::kClassIntB][b_val] = ActiveSet(
+                a_set, setup.permuter_a.permutation_count());
       }
     }
   }
@@ -208,8 +234,8 @@ static void BM_PairBackAndForth(benchmark::State& state) {
         active_set_a.AddFalseBlock(source_a.ConsumeFalseBlock());
         CHECK(source_a.ConsumeNext());
         bool any_of_a = false;
-	ActiveSet a_b_set;
-	ActiveSet source_b = setup.permuter_b.active_set();
+        ActiveSet a_b_set;
+        ActiveSet source_b = setup.permuter_b.active_set();
         for (auto it_b = setup.permuter_b.begin();
              it_b != setup.permuter_b.end();
              ++it_b) {
@@ -223,18 +249,18 @@ static void BM_PairBackAndForth(benchmark::State& state) {
             any_of_a = true;
             if (build_mode == SetupState::kEarlyExit) break;
           }
-	  if (build_mode == SetupState::kMakePairs) {
-	    a_b_set.AddFalseBlock(source_b.ConsumeFalseBlock());
-	    CHECK(source_b.ConsumeNext());
-	    a_b_set.Add(this_match);
-	  }
+          if (build_mode == SetupState::kMakePairs) {
+            a_b_set.AddFalseBlock(source_b.ConsumeFalseBlock());
+            CHECK(source_b.ConsumeNext());
+            a_b_set.Add(this_match);
+          }
         }
         active_set_a.Add(any_of_a);
-	if (build_mode == SetupState::kMakePairs && any_of_a) {
-	  a_b_set.DoneAdding();
-	  setup.active_set_pairs[SetupState::kClassIntA][it_a.position()] =
-	    std::move(a_b_set);
-	}
+        if (build_mode == SetupState::kMakePairs && any_of_a) {
+          a_b_set.DoneAdding();
+          setup.active_set_pairs[SetupState::kClassIntA][it_a.position()] =
+            std::move(a_b_set);
+        }
       }
 
       active_set_a.AddFalseBlock(source_a.ConsumeFalseBlock());
@@ -252,8 +278,8 @@ static void BM_PairBackAndForth(benchmark::State& state) {
         setup.mutable_solution.SetClass(it_b);
         active_set_b.AddFalseBlock(source_b.ConsumeFalseBlock());
         CHECK(source_b.ConsumeNext());
-	ActiveSet b_a_set;
-	ActiveSet source_a = setup.permuter_a.active_set();
+        ActiveSet b_a_set;
+        ActiveSet source_a = setup.permuter_a.active_set();
         bool any_of_b = false;
         for (auto it_a = setup.permuter_a.begin();
              it_a != setup.permuter_a.end();
@@ -268,18 +294,18 @@ static void BM_PairBackAndForth(benchmark::State& state) {
             any_of_b = true;
             if (build_mode == SetupState::kEarlyExit) break;
           }
-	  if (build_mode == SetupState::kMakePairs) {
-	    b_a_set.AddFalseBlock(source_a.ConsumeFalseBlock());
-	    CHECK(source_a.ConsumeNext());
-	    b_a_set.Add(this_match);
-	  }
+          if (build_mode == SetupState::kMakePairs) {
+            b_a_set.AddFalseBlock(source_a.ConsumeFalseBlock());
+            CHECK(source_a.ConsumeNext());
+            b_a_set.Add(this_match);
+          }
         }
         active_set_b.Add(any_of_b);
-	if (build_mode == SetupState::kMakePairs && any_of_b) {
-	  b_a_set.DoneAdding();
-	  setup.active_set_pairs[SetupState::kClassIntB][it_b.position()] =
-	    std::move(b_a_set);
-	}
+        if (build_mode == SetupState::kMakePairs && any_of_b) {
+          b_a_set.DoneAdding();
+          setup.active_set_pairs[SetupState::kClassIntB][it_b.position()] =
+            std::move(b_a_set);
+        }
       }
 
       active_set_b.AddFalseBlock(source_b.ConsumeFalseBlock());
@@ -309,5 +335,15 @@ BENCHMARK_TEMPLATE(BM_PairBackAndForth, SetupState::kFullIteration)->Arg(5);
 BENCHMARK_TEMPLATE(BM_PairSet, SetupState::kMakePairs)->Arg(5);
 BENCHMARK_TEMPLATE(BM_PairPassThroughA, SetupState::kMakePairs)->Arg(5);
 BENCHMARK_TEMPLATE(BM_PairBackAndForth, SetupState::kMakePairs)->Arg(5);
+
+BENCHMARK_TEMPLATE(BM_PairSet, SetupState::kEarlyExit)->Arg(7);
+BENCHMARK_TEMPLATE(BM_PairPassThroughA, SetupState::kEarlyExit)->Arg(7);
+BENCHMARK_TEMPLATE(BM_PairBackAndForth, SetupState::kEarlyExit)->Arg(7);
+BENCHMARK_TEMPLATE(BM_PairSet, SetupState::kFullIteration)->Arg(7);
+BENCHMARK_TEMPLATE(BM_PairPassThroughA, SetupState::kFullIteration)->Arg(7);
+BENCHMARK_TEMPLATE(BM_PairBackAndForth, SetupState::kFullIteration)->Arg(7);
+BENCHMARK_TEMPLATE(BM_PairSet, SetupState::kMakePairs)->Arg(7);
+BENCHMARK_TEMPLATE(BM_PairPassThroughA, SetupState::kMakePairs)->Arg(7);
+BENCHMARK_TEMPLATE(BM_PairBackAndForth, SetupState::kMakePairs)->Arg(7);
 
 }  // namespace puzzle
