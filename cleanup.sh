6e5d420ea5a1baae097c0baaa1115c8476af6cf2
Keith Peters <keith@monkeynova.com>
2020-05-03 14:04:18 -0700

    update cleanup.sh to enforce clang-tidy

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,4 @@
 #!/bin/sh -x
 
-perl -spi -e 's/\t/" "x8/ge' {puzzle/,sudoku/,}*.{cc,h}
-perl -spi -e 's/\s+\n/\n/' {puzzle/,sudoku/,}*.{cc,h}
-rm *~ puzzle/*~ sudoku/*~
+clang-format --style=Google -i `find . -name \*.h -o -name \*.cc`
+rm -f *~ puzzle/*~ sudoku/*~
