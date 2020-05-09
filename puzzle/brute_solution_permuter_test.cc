e16b2159f330d1798f048a239b4fa53a6ac94b90
Keith Peters <keith@monkeynova.com>
2020-05-09 14:41:49 -0700

    standardize on namespace-ing tests to reduce :: operators

--- puzzle/brute_solution_permuter_test.cc
+++ puzzle/brute_solution_permuter_test.cc
@@ -8,22 +8,24 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
+namespace puzzle {
+
 TEST(BruteSolutionPermuterTest, Simple) {
-  puzzle::EntryDescriptor ed;
-  puzzle::IntRangeDescriptor id(3, 5);
-  puzzle::IntRangeDescriptor cd1(6, 8);
-  puzzle::IntRangeDescriptor cd2(11, 13);
+  EntryDescriptor ed;
+  IntRangeDescriptor id(3, 5);
+  IntRangeDescriptor cd1(6, 8);
+  IntRangeDescriptor cd2(11, 13);
 
   ed.SetIds(&id);
   ed.SetClass(0, "foo", &cd1);
   ed.SetClass(1, "bar", &cd2);
 
-  puzzle::BruteSolutionPermuter p(&ed);
+  BruteSolutionPermuter p(&ed);
   p.Prepare();
 
   std::unordered_set<std::string> history;
   EXPECT_THAT(p.permutation_count(), 6 * 6);
-  std::vector<puzzle::Solution> solutions;
+  std::vector<Solution> solutions;
   for (auto it = p.begin(); it != p.end(); ++it) {
     EXPECT_THAT(it.position(), solutions.size());
     EXPECT_THAT(history.insert(it->DebugString()).second, true)
@@ -36,3 +38,5 @@ TEST(BruteSolutionPermuterTest, Simple) {
         << solution.DebugString();
   }
 }
+
+}  // namespace puzzle
