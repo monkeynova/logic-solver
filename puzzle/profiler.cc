bccb235a8246be2aaebdf4b43a9f694aa195a726
Keith Peters <keith@monkeynova.com>
2020-05-17 17:34:58 -0700

    move profiler to portable absl::Time

--- puzzle/profiler.cc
+++ puzzle/profiler.cc
@@ -1,11 +1,10 @@
 #include "puzzle/profiler.h"
 
-#include <sys/time.h>
-
 #include <iomanip>
 #include <iostream>
 
 #include "absl/flags/flag.h"
+#include "absl/time/time.h"
 #include "absl/memory/memory.h"
 
 ABSL_FLAG(int, puzzle_max_profile_calls, std::numeric_limits<int>::max(),
@@ -20,13 +19,13 @@ namespace puzzle {
 
 namespace {
 
-class StructTimevalProfiler : public Profiler {
+class AbslTimeProfiler : public Profiler {
  public:
-  StructTimevalProfiler() : Profiler() {
-    gettimeofday(&start_, nullptr);
+  AbslTimeProfiler() : Profiler() {
+    start_ = absl::Now();
     last_ = start_;
   }
-  ~StructTimevalProfiler() override { std::cout << "\033[1K\r" << std::flush; }
+  ~AbslTimeProfiler() override { std::cout << "\033[1K\r" << std::flush; }
 
  private:
   bool Done() override {
@@ -34,34 +33,31 @@ class StructTimevalProfiler : public Profiler {
   }
 
   bool NotePositionImpl(double position, double count) override {
-    struct timeval now;
-    gettimeofday(&now, nullptr);
-    double delta =
-        (now.tv_sec - last_.tv_sec + 1e-6 * (now.tv_usec - last_.tv_usec));
-    if (delta < 0.2) return false;
+    absl::Time now = absl::Now();
+    int delta = (now - last_) / absl::Microseconds(1);
+    if (delta < 200) return false;
 
-    double full_delta =
-        (now.tv_sec - start_.tv_sec + 1e-6 * (now.tv_usec - start_.tv_usec));
+    int full_delta = (now - start_) / absl::Microseconds(1);
 
-    double qps = (position - last_position_) / delta;
+    double permutations_per_milli = (position - last_position_) / delta;
     std::cout << std::setprecision(3) << "\033[1K\rTrying "
-              << (100 * position / count) << "%, effective=" << qps / 1000
-              << "Kqps true=" << (test_calls() / full_delta / 1000) << "Kqps"
+              << (100 * position / count) << "%, effective="
+	      << permutations_per_milli
+              << "Kp/ms true=" << (test_calls() / full_delta) << "Kp/ms"
               << std::flush;
     last_ = now;
     last_position_ = position;
     return true;
   }
 
-  void NoteFinish() override { gettimeofday(&last_, nullptr); }
+  void NoteFinish() override { last_ = absl::Now(); }
 
   double Seconds() override {
-    return last_.tv_sec - start_.tv_sec +
-           1e-6 * (last_.tv_usec - start_.tv_usec);
+    return 1e-3 * ((last_ - start_) / absl::Milliseconds(1));
   }
 
-  struct timeval start_;
-  struct timeval last_;
+  absl::Time start_;
+  absl::Time last_;
   double last_position_;
 };
 
@@ -72,7 +68,7 @@ std::unique_ptr<Profiler> Profiler::Create() {
     return nullptr;
   }
 
-  return absl::make_unique<StructTimevalProfiler>();
+  return absl::make_unique<AbslTimeProfiler>();
 }
 
 }  // namespace puzzle
