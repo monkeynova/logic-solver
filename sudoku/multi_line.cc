cc9ba92b4722851b58fb597ec765abdf0926fdee
Keith Peters <keith@monkeynova.com>
2020-05-17 17:35:22 -0700

    add timing to multi_line output

--- sudoku/multi_line.cc
+++ sudoku/multi_line.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <memory>
 
+#include "absl/time/time.h"
 #include "puzzle/main_lib.h"
 #include "sudoku/line_board.h"
 
@@ -12,6 +13,7 @@ int main(int argc, char** argv) {
 
   for (std::string buf; std::getline(std::cin, buf);) {
     std::cout << "In:  " << buf << std::endl;
+    absl::Time start = absl::Now();
     std::unique_ptr<::puzzle::Problem> line_board =
         ::sudoku::LineBoard::Create(buf);
     CHECK(line_board != nullptr) << "No puzzle found";
@@ -20,8 +22,10 @@ int main(int argc, char** argv) {
 
     ::puzzle::Solution answer = line_board->Solve();
     if (answer.IsValid()) {
+      absl::Time done = absl::Now();
       std::cout << "Out: " << ::sudoku::LineBoard::ToString(answer)
-                << std::endl;
+                << " (" << 1e-3 * ((done - start) / absl::Milliseconds(1)) << "s)"
+		<< std::endl;
     } else {
       LOG(ERROR) << "No answer found";
       exit_code = 1;
