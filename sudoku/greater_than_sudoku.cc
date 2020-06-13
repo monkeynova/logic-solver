4877105c6cfc97d1734c75bd9e31ddb9c2070d90
Keith Peters <keith@monkeynova.com>
2020-06-13 11:57:16 -0700

    there appears to be an implicit no-comparisons-across-boxes rule given all the problems I have looked at

--- sudoku/greater_than_sudoku.cc
+++ sudoku/greater_than_sudoku.cc
@@ -7,6 +7,10 @@ void GreaterThanSudoku::AddComparison(const std::pair<Box, Box>& cmp) {
                abs(cmp.first.class_id - cmp.second.class_id),
            1)
       << "Comparison must be on neighboring boxes";
+  CHECK_EQ(cmp.first.entry_id / 3, cmp.second.entry_id / 3)
+    << "Comparisons must be with in a box (entry)";
+  CHECK_EQ(cmp.first.class_id / 3, cmp.second.class_id / 3)
+    << "Comparisons must be with in a box (class)";
   AddPredicate(
       absl::StrCat(cmp.first.DebugString(), " > ", cmp.second.DebugString()),
       [cmp](const puzzle::Solution& s) {
