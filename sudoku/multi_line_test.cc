e29fad6a6a0224ffa38076635f0d062434c4006f
Keith Peters <keith@monkeynova.com>
2020-09-15 07:30:43 -0700

    Force compilation to be C++17 compliant which then allows TestCaseOptions to be usable

--- sudoku/multi_line_test.cc
+++ sudoku/multi_line_test.cc
@@ -5,7 +5,7 @@
 // There are missing implicit casts from absl::string_view to std::string.
 // Even better would be that since the places it's used don't need a copy, we
 // could just convert the uses to absl::string_views.
-#define USE_OPTIONS 0
+#define USE_OPTIONS 1
 
 #include "absl/strings/ascii.h"
 #include "absl/strings/str_join.h"
@@ -14,18 +14,22 @@
 #include "gtest/gtest.h"
 #include "run_test_case_result.h"
 #include "sudoku/line_board.h"
-#if USE_OPTIONS
 #include "test_case_options.h"
-#endif
+
+// If set, flags the test suite to ignore the result.
+static inline constexpr absl::string_view kIgnoreThisTestOption =
+    "ignore_this_test";
 
 void TestLineBoard(absl::string_view test_case,
                    file_based_test_driver::RunTestCaseResult* test_result) {
   // Parse and strip off the test case's options.
   std::string test_case_without_options = std::string(test_case);
 
-#if USE_OPTIONS
+  file_based_test_driver::TestCaseOptions options;
+  options.RegisterBool(kIgnoreThisTestOption, false);
+
   const absl::Status options_status =
-      options_.ParseTestCaseOptions(&test_case_without_options);
+      options.ParseTestCaseOptions(&test_case_without_options);
   if (!options_status.ok()) {
     // For bad test cases, prefer to return an error in the output instead
     // of crashing.
@@ -36,11 +40,10 @@ void TestLineBoard(absl::string_view test_case,
 
   // Ignore? Then return straight away. The test driver will copy the entire
   // test case verbatim.
-  if (options_.GetBool(kIgnoreThisTestOption)) {
+  if (options.GetBool(kIgnoreThisTestOption)) {
     test_result->set_ignore_test_output(true);
     return;
   }
-#endif
 
   absl::StripAsciiWhitespace(&test_case_without_options);
 
@@ -63,6 +66,5 @@ void TestLineBoard(absl::string_view test_case,
 
 TEST(MultiLineTest, FileBasedTest) {
   EXPECT_TRUE(file_based_test_driver::RunTestCasesFromFiles(
-      absl::StrCat("sudoku/", "sudoku17_sample.test"),
-      TestLineBoard));
+      absl::StrCat("sudoku/", "sudoku17_sample.test"), TestLineBoard));
 }
