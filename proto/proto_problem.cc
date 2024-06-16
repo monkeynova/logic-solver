9c84fbd4d7d9a0d12754ac5bf9b9a892f9cda6ca
Keith Peters <keith@monkeynova.com>
2024-06-16 08:45:22 -0700

    Remove duplicate sanity checks in ProtoPuzzle and move a bit closer to RAII

--- proto/proto_problem.cc
+++ proto/proto_problem.cc
@@ -5,139 +5,100 @@
 
 namespace puzzle {
 
-static absl::StatusOr<EntryDescriptor> MakeEntryDescriptor(
+// static
+absl::StatusOr<ProtoProblemBase::Init> ProtoProblemBase::MakeInit(
     const google::protobuf::Descriptor* proto_descriptor) {
+  Init ret = {
+    .problem_descriptor = proto_descriptor,
+    .entry_descriptor = EntryDescriptor(nullptr, nullptr, {}),
+  };
+
   if (proto_descriptor->field_count() != 1) {
     return absl::InvalidArgumentError("Proto must have a single field");
   }
-  const google::protobuf::FieldDescriptor* entry_field =
-      proto_descriptor->field(0);
-  if (entry_field->label() !=
+  ret.entry_field = proto_descriptor->field(0);
+  if (ret.entry_field->label() !=
       google::protobuf::FieldDescriptor::Label::LABEL_REPEATED) {
     return absl::InvalidArgumentError("Proto field must be a repeated");
   }
-  const google::protobuf::Descriptor* entry_descriptor =
-      entry_field->message_type();
-  if (entry_descriptor == nullptr) {
+  ret.entry_message_descriptor = ret.entry_field->message_type();
+  if (ret.entry_message_descriptor == nullptr) {
     return absl::InvalidArgumentError("Proto field must be of type Message");
   }
 
-  const google::protobuf::FieldDescriptor* id_field =
-      entry_descriptor->FindFieldByNumber(1);
-  if (id_field == nullptr) {
+  ret.id_field = ret.entry_message_descriptor->FindFieldByNumber(1);
+  if (ret.id_field == nullptr) {
     return absl::InvalidArgumentError("Entry field must have field number 1");
   }
-  if (id_field->name() != "id") {
+  if (ret.id_field->name() != "id") {
     return absl::InvalidArgumentError(
         "Entry field with tag 1 must be named id");
   }
-  const google::protobuf::EnumDescriptor* id_enum = id_field->enum_type();
+  const google::protobuf::EnumDescriptor* id_enum = ret.id_field->enum_type();
   if (id_enum == nullptr) {
     return absl::InvalidArgumentError("id field must be of type enum");
   }
 
-  std::vector<std::string> class_names(entry_descriptor->field_count() - 1);
-  std::vector<std::unique_ptr<const Descriptor>> class_descriptors(
-      entry_descriptor->field_count() - 1);
-  for (int i = 0; i < entry_descriptor->field_count(); ++i) {
-    const google::protobuf::FieldDescriptor* field = entry_descriptor->field(i);
-    if (field == id_field) {
+  int class_field_count = ret.entry_message_descriptor->field_count() - 1;
+  std::vector<std::string> class_names(class_field_count);
+  std::vector<std::unique_ptr<const Descriptor>> class_descriptors(class_field_count);
+  ret.class_fields.clear();
+  ret.class_fields.reserve(class_field_count);
+  for (int i = 0; i < ret.entry_message_descriptor->field_count(); ++i) {
+    const google::protobuf::FieldDescriptor* field = ret.entry_message_descriptor->field(i);
+    if (field == ret.id_field) {
       continue;
     }
     const google::protobuf::EnumDescriptor* enum_type = field->enum_type();
     if (enum_type == nullptr) {
       return absl::InvalidArgumentError("fields must be of type enum");
     }
-    // TODO(@monkeynova): -2 here takes the first non-id field (2) and maps it
-    // to the first class (0). This is an awful, hard-coded translation and a
-    // better data model should be found.
-    class_names[field->number() - 2] = field->name();
-    class_descriptors[field->number() - 2] =
+
+    class_names[ret.class_fields.size()] = field->name();
+    class_descriptors[ret.class_fields.size()] =
         absl::make_unique<ProtoEnumDescriptor>(enum_type);
+    ret.class_fields.push_back(field);
   }
 
-  EntryDescriptor puzzle_entry_desriptor(
+  ret.entry_descriptor = EntryDescriptor(
       absl::make_unique<ProtoEnumDescriptor>(id_enum),
       absl::make_unique<StringDescriptor>(class_names),
       std::move(class_descriptors));
 
-  return puzzle_entry_desriptor;
-}
-
-static EntryDescriptor CheckDescriptor(
-    absl::StatusOr<EntryDescriptor> entry_descriptor) {
-  // TODO(@monkeynova): Maybe find a way to defer until setup...
-  CHECK(entry_descriptor.ok()) << entry_descriptor.status();
-  return std::move(*entry_descriptor);
+  return ret;
 }
 
 ProtoProblemBase::ProtoProblemBase(
-    const google::protobuf::Descriptor* problem_descriptor)
-    : Problem(CheckDescriptor(MakeEntryDescriptor(problem_descriptor))),
-      problem_descriptor_(problem_descriptor) {}
+    absl::StatusOr<ProtoProblemBase::Init> init)
+  : Problem(std::move(init->entry_descriptor)),
+    problem_descriptor_(init->problem_descriptor),
+    entry_field_(init->entry_field),
+    entry_message_descriptor_(init->entry_message_descriptor),
+    id_field_(init->id_field),
+    class_fields_(std::move(init->class_fields)) {}
 
 absl::StatusOr<Solution> ProtoProblemBase::GetSolution() const {
   google::protobuf::DynamicMessageFactory factory;
   const google::protobuf::Descriptor* proto_descriptor = problem_descriptor();
 
-  std::unique_ptr<google::protobuf::Message> solution =
-      absl::WrapUnique(factory.GetPrototype(proto_descriptor)->New());
+  std::unique_ptr<google::protobuf::Message> solution(
+      factory.GetPrototype(proto_descriptor)->New());
   if (!google::protobuf::TextFormat::ParseFromString(solution_textproto(),
                                                      solution.get())) {
     return absl::InvalidArgumentError("Could not parse proto");
   }
 
-  if (proto_descriptor->field_count() != 1) {
-    return absl::InvalidArgumentError("Proto must have a single field");
-  }
-  const google::protobuf::FieldDescriptor* entry_field =
-      proto_descriptor->field(0);
-  if (entry_field->label() !=
-      google::protobuf::FieldDescriptor::Label::LABEL_REPEATED) {
-    return absl::InvalidArgumentError("Proto field must be a repeated");
-  }
-  const google::protobuf::Descriptor* entry_proto = entry_field->message_type();
-  if (entry_proto == nullptr) {
-    return absl::InvalidArgumentError("Proto field must be of type Message");
-  }
-
-  const google::protobuf::FieldDescriptor* id_field =
-      entry_proto->FindFieldByNumber(1);
-  if (id_field == nullptr) {
-    return absl::InvalidArgumentError("Entry field must have field number 1");
-  }
-  if (id_field->name() != "id") {
-    return absl::InvalidArgumentError(
-        "Entry field with tag 1 must be named id");
-  }
-  const google::protobuf::EnumDescriptor* id_enum = id_field->enum_type();
-  if (id_enum == nullptr) {
-    return absl::InvalidArgumentError("id field must be of type enum");
-  }
-
   const google::protobuf::RepeatedFieldRef<
       google::protobuf::Message>& proto_entries =
       solution->GetReflection()->GetRepeatedFieldRef<google::protobuf::Message>(
-          *solution, entry_field);
+          *solution, entry_field_);
 
   std::vector<Entry> entries;
   for (const auto& entry : proto_entries) {
-    int id = entry.GetReflection()->GetEnumValue(entry, id_field);
+    int id = entry.GetReflection()->GetEnumValue(entry, id_field_);
     std::vector<int> class_values(entry_descriptor()->AllClasses()->size(), 0);
-    for (int i = 0; i < entry_proto->field_count(); ++i) {
-      const google::protobuf::FieldDescriptor* field = entry_proto->field(i);
-      if (field == id_field) {
-        continue;
-      }
-      const google::protobuf::EnumDescriptor* enum_type = field->enum_type();
-      if (enum_type == nullptr) {
-        return absl::InvalidArgumentError("fields must be of type enum");
-      }
-      // TODO(@monkeynova): -2 here takes the first non-id feild (2) and maps it
-      // to the first class (0). This is an awful, hard-coded translation and a
-      // better data model should be found.
-      class_values[field->number() - 2] =
-          entry.GetReflection()->GetEnumValue(entry, field);
+    for (int i = 0; i < class_fields_.size(); ++i) {
+      class_values[i] = entry.GetReflection()->GetEnumValue(entry, class_fields_[i]);
     }
     entries.emplace_back(id, std::move(class_values), entry_descriptor());
   }
--- proto/proto_problem.h
+++ proto/proto_problem.h
@@ -9,7 +9,8 @@ namespace puzzle {
 class ProtoProblemBase : public Problem {
  public:
   explicit ProtoProblemBase(
-      const google::protobuf::Descriptor* problem_descriptor);
+      const google::protobuf::Descriptor* problem_descriptor)
+    : ProtoProblemBase(MakeInit(problem_descriptor)) {}
 
   absl::Status Setup() override;
   absl::StatusOr<puzzle::Solution> GetSolution() const override;
@@ -21,7 +22,24 @@ class ProtoProblemBase : public Problem {
   virtual std::string solution_textproto() const = 0;
 
  private:
+  struct Init {
+    const google::protobuf::Descriptor* problem_descriptor;
+    EntryDescriptor entry_descriptor;
+    const google::protobuf::FieldDescriptor* entry_field;
+    const google::protobuf::Descriptor* entry_message_descriptor;
+    const google::protobuf::FieldDescriptor* id_field;
+    std::vector<const google::protobuf::FieldDescriptor*> class_fields;
+  };
+  explicit ProtoProblemBase(absl::StatusOr<ProtoProblemBase::Init> init);
+
+  static absl::StatusOr<Init> MakeInit(
+      const google::protobuf::Descriptor* proto_descriptor);
+
   const google::protobuf::Descriptor* problem_descriptor_;
+  const google::protobuf::FieldDescriptor* entry_field_;
+  const google::protobuf::Descriptor* entry_message_descriptor_;
+  const google::protobuf::FieldDescriptor* id_field_;
+  std::vector<const google::protobuf::FieldDescriptor*> class_fields_;
 };
 
 template <class Proto>
