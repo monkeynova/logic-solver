6fdaef43a23a92d6cc588db2a358544fbe2be71d
Keith Peters <keith@monkeynova.com>
2024-06-23 11:24:20 -0700

    Move multi_line to show timing only by default

--- sudoku/multi_line.cc
+++ sudoku/multi_line.cc
@@ -7,6 +7,33 @@
 #include "main_lib.h"
 #include "sudoku/line_board.h"
 
+class Stats {
+ public:
+  Stats() = default;
+
+  void Add(int point) {
+    max_ = std::max(point, max_);
+    min_ = std::min(point, min_);
+    ++count_;
+    sum_ += point;
+  }
+
+  int min() const { return min_; }
+  int max() const { return max_; }
+  int avg() const { return sum_ / count_; }
+
+  template <typename Sink>
+  friend void AbslStringify(Sink& s, const Stats& st) {
+    absl::Format(&s, "%d/%d/%d (%d)", st.min(), st.avg(), st.max(), st.count_);
+  }
+
+ private:
+  int min_ = std::numeric_limits<int>::max();
+  int max_ = std::numeric_limits<int>::min();
+  int count_ = 0;
+  int sum_ = 0;
+};
+
 int main(int argc, char** argv) {
   std::vector<char*> args = InitMain(
       argc, argv,
@@ -15,9 +42,12 @@ int main(int argc, char** argv) {
                             << absl::ProgramUsageMessage();
 
   int exit_code = 0;
+  Stats time_stats;
+
+  absl::Time last_flush = absl::Now();
 
   for (std::string buf; std::getline(std::cin, buf);) {
-    std::cout << "In:  " << buf << std::endl;
+    LOG(INFO) << "In:  " << buf << std::endl;
     absl::Time start = absl::Now();
     ::sudoku::LineBoard line_board(buf);
 
@@ -28,14 +58,21 @@ int main(int argc, char** argv) {
     QCHECK(answer.ok()) << answer.status();
     if (answer->IsValid()) {
       absl::Time done = absl::Now();
-      std::cout << "Out: " << ::sudoku::LineBoard::ToString(*answer) << " ("
-                << 1e-3 * ((done - start) / absl::Milliseconds(1)) << "s)"
-                << std::endl;
+      int solve_ms = (done - start) / absl::Milliseconds(1);
+      time_stats.Add(solve_ms);
+      if (done - last_flush > absl::Milliseconds(250)) {
+        std::cout << absl::StreamFormat("\033[1K\r%v", time_stats) << std::flush;
+        last_flush = done;
+      }
+
+      LOG(INFO) << "Out: " << ::sudoku::LineBoard::ToString(*answer) << " ("
+                << solve_ms << "ms)";
     } else {
       LOG(ERROR) << "No answer found";
       exit_code = 1;
     }
   }
+  std::cout << std::endl;
 
   return exit_code;
 }
